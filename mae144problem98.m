2dd9e457cdac5c590021b1babe17f1ef1534e6b8
Judy021 <109991556+Judy021@users.noreply.github.com>
2022-10-19 13:04:52 -0700

    Update mae144problem98.m

--- mae144problem98.m
+++ mae144problem98.m
@@ -17,18 +17,49 @@ den = [M*m, C*m+M*c+c*m, k*m+C*c+K*m+M*k, C*k+K*c, K*k];
 num = [C*m, C*c+K*m, C*k+K*c, K*k];
 system = RR_tf(num, den)
 
+%% Problem 9.8 part d
+clear
+
+syms  s k c
+m=0;
+M=10;
+K=10;
+C=0;
+L1 = M*s^2 + C*s + K
+L2 = K + C*s
+L3 = m*s^2 + k + c*s
+L4 = k + c*s
+
+L5 = expand(L1*L3-L4^2+L4*L3)
+L6 = expand(L2*L3)
+sys1=tf([1],[1 0 1])
+Poles=pole(sys1)
+Zeroes=zero(sys1)
+figure(1)
+bode(sys1)
+
+%% Problem 9.8 part e
+
+figure(2)
+step(sys1)
+sys2=tf([1],[1 0 1 0])
+Poles=pole(sys2)
+Zeroes=zero(sys2)
+
+figure(3)
+step(sys2)
 
 %% Prooblem 9.8 part f 
 clear
 
 [Z,P,K] = tf2zp([sqrt(2),1],[1,sqrt(2), 1])
-figure(1)
+figure(5)
 bode([sqrt(2),1],[1,sqrt(2), 1])
 
-figure(2)
+figure(6)
 impulse([sqrt(2),1],[1,sqrt(2), 1])
 
-figure(3)
+figure(7)
 step([sqrt(2),1],[1,sqrt(2), 1,0])
 %% Problem 9.8 part g
 clear
